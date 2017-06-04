aa1a42b766e12395c53ccbb445e742251e92d667
tcsx <username@users.noreply.github.com>
2017-06-04 11:48:52 +0800

    complete NBody class

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,3 +1,18 @@
 public class NBody {
+    public static double readRadius(String fileName) {
+        In in = new In(fileName);
+        in.readInt();
+        return in.readDouble();
+    }
 
+    public static Planet[] readPlanets(String fileName) {
+        In in = new In(fileName);
+        int n = in.readInt(); 
+        Planet [] aPlanets = new Planet[n];
+        in.readDouble();
+        for (int i = 0; i < n; i++) {
+            aPlanets[i] = new Planet(in.readDouble(), in.readDouble(), in.readDouble(), in.readDouble(), in.readDouble(), in.readString());
+        }
+        return aPlanets;
+    }
 }
